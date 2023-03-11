0283087427c3afbf7ac9734efffaf82723dc16c9
30Piraten <kioyarautenberg@gmail.com>
2023-03-11 07:38:02 +0200

    Add truthy tests

--- src/truthiness.test.js
+++ src/truthiness.test.js
@@ -9,3 +9,34 @@ test('function that returns null', () => {
     expect(result).not.toBeTruthy();
     expect(result).toBeFalsy();
 })
+
+
+test('if a value is true', () => {
+    let result = 'user'
+
+    if (result !== null) true;
+
+    expect(result).toBeTruthy();
+});
+
+test('if a value is true', () => {
+    let code = 'whack'
+
+    if ('meh!' !== code) true;
+
+    expect(code).toBeTruthy();
+});
+
+test('if a value is false', () => {
+    let a = '';
+    let b = null;
+    let c = undefined;
+    let d = 0;
+    let e = NaN;
+
+    expect(a).toBeFalsy();
+    expect(b).toBeFalsy();
+    expect(c).toBeFalsy();
+    expect(d).toBeFalsy();
+    expect(e).toBeFalsy();
+});
